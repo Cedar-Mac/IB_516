e1728eb149c6306fda9124618b618ff504073c50
Cedar Mackaness <cedarmkns@gmail.com>
2025-01-26 10:56:59 -0800

    add function description for merge_pairs.py

--- code/Python/merge_pairs.py
+++ code/Python/merge_pairs.py
@@ -1,6 +1,30 @@
-import os, re, subprocess, concurrent
+import os, re, subprocess
 
 def merge_pairs(data_dir:str):
+    """
+    Merges paired fastq reads using vsearch algorithm.
+
+    Arguments:
+
+        data_dir: string providing the path to the data directory with raw input fastq files.
+
+    vsearch arguments:
+
+    --fastq_mergepairs: specify the maximum number of non-matching nucleotides 
+                        allowed in the overlap region. That option has a strong 
+                        influence on the merging success rate. Set to 99 a la JAMP.
+    --fastq_minovlen: Minimum overlap between paired sequences to merge. JAMP default is 16,
+                        base default is 10. Currently set to JAMP default.
+    --fastq_diffpct: Maximum allowed percent difference between the two paired sequences. 
+                        vsearch default is 100%, JAMP sets the default to 25%
+                        I'm using vsearch default.
+    --fastq_allowmergestagger: Allows for reads to be staggered, this is following JAMP convention.
+
+    Output:
+
+    Merged fastq files in merged directory within data directory.
+    """
+
     data_files = os.listdir(data_dir)
 
     fastq_suffix = "_RX.fastq"
@@ -9,12 +33,11 @@ def merge_pairs(data_dir:str):
     fwd_files = [fname for fname in data_files if re.search(pattern = "_R1.fastq", string = fname)]
     rvs_files = [fname for fname in data_files if re.search(pattern = "_R2.fastq", string = fname)]
 
-    # Check that all reads are paired
-    only_in_fwd_reads = [s for s in fwd_files if any(substr in s for substr in rvs_files)]
-    only_in_rvs_reads = [s for s in fwd_files if any(substr in s for substr in rvs_files)]
-    unmatched_files = only_in_fwd_reads + only_in_rvs_reads
+    # Check that all reads are paired 
+    fwd_bare_names = set([name[0:-len(fastq_suffix)] for name in fwd_files])
+    rvs_bare_names = set([name[0:-len(fastq_suffix)] for name in rvs_files])
 
-    if unmatched_files:
+    if fwd_bare_names != rvs_bare_names:
         print("Not all reads are paired!")
         exit()
 
@@ -26,10 +49,17 @@ def merge_pairs(data_dir:str):
 
     # Use VSEARCH fastq_mergepairs function
     for fastq_name in fwd_files:
-        vsearch_merge_call = ["vsearch", "--fastq_mergepairs", f"{data_dir}/{fastq_name}", 
+        vsearch_merge_call = ["vsearch", 
+                                "--fastq_mergepairs", f"{data_dir}/{fastq_name}", 
                                 "--reverse", f"{data_dir}/{fastq_name}", 
-                                #"--eetabbedout", f"{data_dir}/logs/{fastq_name}.log", 
-                                "--fastqout", f"{data_dir}/merged/{fastq_name[0:-len(fastq_suffix)]}_merged.fastq"]
+                                "--fastqout", f"{data_dir}/merged/{fastq_name[0:-len(fastq_suffix)]}_merged.fastq",
+                                #"-log", f"{data_dir}/logs/{fastq_name}[0:-len(fastq_suffix)].log", 
+                                "--fastq_maxdiffs", "99", 
+                                "--fastq_minovlen", "16",
+                                "--fastq_maxdiffpct", "25",
+                                "--fastq_allowmergestagger"]
+
+
         subprocess.check_call(vsearch_merge_call)
 
 
--- code/Python/trim_primers.py
+++ code/Python/trim_primers.py
@@ -1,8 +1,12 @@
-def trim_primers(primer_option:int):
+import subprocess
+
+def trim_primers(merged_dir: str, primer_option:int):
     fwd_primer = ""
     rvs_primer = ""
     
     if primer_option == 1:
-        fwd_primer = "GGDACWGGWTGAACWGTWTAYCCHCC "
+        fwd_primer = "GGDACWGGWTGAACWGTWTAYCCHCC"
         rvs_primer = "CAAACAAATARDGGTATTCGDTY"
     
+    for file in merged_dir:
+        cutadapt_call = [""]
