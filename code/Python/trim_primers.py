63a1806defd534a801b22c1aed2c8ae5fbdc1282
Cedar Mackaness <cedarmkns@gmail.com>
2025-01-26 19:47:44 -0800

    trim_primers.py now trims, but wrong primer setup

--- code/Python/trim_primers.py
+++ code/Python/trim_primers.py
@@ -1,12 +1,25 @@
-import subprocess
+import os, subprocess
 
-def trim_primers(merged_dir: str, primer_option:int):
+def trim_primers(data_dir: str, primer_option:int=1):
+    merged_suffix = "_merged.fastq"
     fwd_primer = ""
     rvs_primer = ""
     
+    # default primer options uses the EPT COI primer set
     if primer_option == 1:
         fwd_primer = "GGDACWGGWTGAACWGTWTAYCCHCC"
         rvs_primer = "CAAACAAATARDGGTATTCGDTY"
     
-    for file in merged_dir:
-        cutadapt_call = [""]
+    # make output directory for trimmed files
+    if "trimmed" not in os.listdir(data_dir):
+        os.makedirs(f"{data_dir}/trimmed/")
+
+    for file in os.listdir(f"{data_dir}/merged/"):
+        cutadapt_call = ["cutadapt", 
+                         "-a", f"^{fwd_primer}...{rvs_primer}$", 
+                         "-o", f"{data_dir}/trimmed/{file[0:-len(merged_suffix)]}_trimmed.fastq", 
+                         f"{data_dir}/merged/{file}"]
+        
+        subprocess.check_call(cutadapt_call)
+
+trim_primers(data_dir="../../data/test_data/")
